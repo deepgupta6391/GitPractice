2b7cae11a31f43167583ddd80cd08d1f4f313096
Deepshikha Gupta <deepshikhagupta@Deepshikhas-Air.fios-router.home>
2021-02-19 20:04:02 -0500

    change3

--- src/main/java/prac1/RegisterationPage.java
+++ src/main/java/prac1/RegisterationPage.java
@@ -5,14 +5,12 @@ public class RegisterationPage {
 	public void username() {
 		System.out.println("entering username");
 		System.out.println("again entering username");
-<<<<<<< HEAD
 
-=======
->>>>>>> 347d9824948372a8c4a19a176be6600fec7e46f2
 	}
 	
 	public void password(){
 		System.out.println("entering password");
 		System.out.println("again entering password");
+		System.out.println("Password should not be spcial char");
 	}
 }
