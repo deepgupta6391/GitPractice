aa02a53814f0fad8187261757d94a5f452cfff55
Deepshikha Gupta <deepshikhagupta@Deepshikhas-Air.fios-router.home>
2021-02-19 19:40:35 -0500

    adding change 2

--- src/main/java/prac1/RegisterationPage.java
+++ src/main/java/prac1/RegisterationPage.java
@@ -4,10 +4,12 @@ public class RegisterationPage {
 
 	public void username() {
 		System.out.println("entering username");
+	
 	}
 	
 	public void password(){
 
 		System.out.println("entering password");
+		System.out.println("again entering password");
 	}
 }
